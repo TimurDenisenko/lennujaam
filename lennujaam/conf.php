d6572c6426d255dd52c9d09281cd0020a20f1af7
Edvard Datser <edvard.datser06@gmail.com>
2024-05-06 10:59:36 +0300

    Kustutas mittetöötavad failid ja muutis conf.php

--- lennujaam/conf.php
+++ lennujaam/conf.php
@@ -1,7 +1,8 @@
 <?php
-$kasutaja = 'd123172_timur';
-$serverinimi = 'd123172.mysql.zonevs.eu';
-$parool='56360881Tt.';
-$andmebaas='d123172_andmebaas';
-$yhendus=new mysqli($serverinimi,$kasutaja,$parool,$andmebaas);
+$kasutaja = 'edvardd';
+$serverinimi = 'localhost';
+$parool = '';
+$andmebaas = 'lenujaam';
+$yhendus = new mysqli($serverinimi, $kasutaja, $parool, $andmebaas);
 $yhendus->set_charset('UTF8');
+?>
